7218a450ff9c9bdbbe50bb91ec8070c0d8ceb5cd
souravmahanty <souravmahanty12@gmail.com>
2022-09-14 17:44:57 +0530

    done

---
+++ PATH_MODULE/index.js
@@ -0,0 +1,7 @@
+
+const path=require('path');
+
+console.log(path.dirname("/home/ongraph/Desktop/ME/NODE-JS/PATH_MODULE/index.js"))
+console.log(path.extname("/home/ongraph/Desktop/ME/NODE-JS/PATH_MODULE/index.js"))
+console.log(path.basename("/home/ongraph/Desktop/ME/NODE-JS/PATH_MODULE/index.js"))
+console.log(path.parse("/home/ongraph/Desktop/ME/NODE-JS/PATH_MODULE/index.js"))

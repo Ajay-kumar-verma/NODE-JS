cd366afbdfc18abc0ab10b5194bfb784c30d2b6a
souravmahanty <souravmahanty12@gmail.com>
2022-09-15 15:49:14 +0530

    done

--- MYSQL_CONNECTION/index.js
+++ MYSQL_CONNECTION/index.js
@@ -2,13 +2,13 @@ const  mysql = require('mysql');
 
 const con = mysql.createConnection({ host: "localhost", user: "root", password: "" });
 
-con.connect(err=> {
-  if(err) throw err;
-   console.log("Connected!");
-})
+// con.connect(err=> {
+//   if(err) throw err;
+//    console.log("Connected!");
+// })
 
 
-con.query("CREATE DATABASE rahul ", function (err, result) {
+con.query("CREATE DATABASE xxxxxxx ", function (err, result) {
     if (err) throw err;
     console.log("Database created");
   });
